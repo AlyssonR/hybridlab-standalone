2ae5e8d65dcf5398f1ea9885f7ee521b9dc2927d
Alysson Raniere <alysson_r076@hotmail.com>
2014-11-05 12:09:39 -0200

    Starting UI creation.

--- src/main/br/hybridlab/standalone/controller/Controller.java
+++ src/main/br/hybridlab/standalone/controller/Controller.java
@@ -1,4 +1,17 @@
 package main.br.hybridlab.standalone.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField potencia;
+
+    @FXML
+    private Button iniciar;
+
+
+
 }
