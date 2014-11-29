5bddf69be2e3d0e0bf4f601119097c6105008952
Alysson Raniere <alysson_r076@hotmail.com>
2014-11-29 15:11:55 -0200

    Creating controller with injected dao and combobox

--- src/main/java/br/hybridlab/standalone/HybridLab.java
+++ src/main/java/br/hybridlab/standalone/HybridLab.java
@@ -6,8 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import org.hibernate.SessionFactory;
 
+import java.lang.reflect.Constructor;
 import java.net.URL;
 
 public class HybridLab extends Application {
@@ -29,7 +31,28 @@ public class HybridLab extends Application {
 
         URL resource = getClass().getResource("/pagina_inicial.fxml");
 
-        Parent root = FXMLLoader.load(resource);
+//        Parent root = FXMLLoader.load(resource);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/pagina_inicial.fxml"));
+        loader.setControllerFactory(new Callback<Class<?>, Object>() {
+            @Override
+            public Object call(Class<?> type) {
+                try {
+                    for (Constructor<?> constructor : type.getConstructors()) {
+                        if (constructor.getParameterCount()==1 &&
+                                constructor.getParameterTypes()[0]==CarDAO.class) {
+                            return constructor.newInstance(carDAO);
+                        }
+                    }
+                    // no matching constructor found, just call no-arg constructor as default:
+                    return type.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null ;
+                }
+            }
+        });
+        Parent root = loader.<Parent>load();
 
         Scene scene = new Scene(root, 1146, 796);
 
--- src/main/java/br/hybridlab/standalone/controller/Controller.java
+++ src/main/java/br/hybridlab/standalone/controller/Controller.java
@@ -1,16 +1,30 @@
 package br.hybridlab.standalone.controller;
 
+import br.hybridlab.standalone.dao.CarDAO;
+import br.hybridlab.standalone.model.Car;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+
+import javax.swing.*;
 
 public class Controller {
 
-    @FXML
-    private TextField potencia;
+    private CarDAO carDAO;
 
     @FXML
-    private Button iniciar;
+    private ComboBox<String> comboExperimentCarModel;
+
+    @FXML
+    public void initialize() {}
+
+    public Controller() {}
+
+    public Controller(CarDAO carDAO) {
+        this.carDAO = carDAO;
+    }
+
 
 
 }
