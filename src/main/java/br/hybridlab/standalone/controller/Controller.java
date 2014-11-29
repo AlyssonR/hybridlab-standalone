ab3c87d6b770e0ba003f51c1c8e849a9841154f3
Alysson Raniere <alysson_r076@hotmail.com>
2014-11-29 16:05:43 -0200

    Setting car details based on selection

--- src/main/java/br/hybridlab/standalone/controller/Controller.java
+++ src/main/java/br/hybridlab/standalone/controller/Controller.java
@@ -2,12 +2,16 @@ package br.hybridlab.standalone.controller;
 
 import br.hybridlab.standalone.dao.CarDAO;
 import br.hybridlab.standalone.model.Car;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -21,7 +25,23 @@ public class Controller {
     private ComboBox<String> comboExperimentCarModel;
 
     @FXML
+    private Text txValueMass;
+
+    @FXML
+    private Text txValuePower;
+
+    @FXML
+    private Text txValueDragCoefficient;
+
+    @FXML
+    private Text txValueFrontalArea;
+
+    @FXML
+    private Text txValueSpeed;
+
+    @FXML
     public void initialize() {
+        //Car models ComboBox:
         comboExperimentCarModel.setPromptText("Escolha um modelo...");
         List<Car> carList = carDAO.get();
         ArrayList<String> modelList = new ArrayList<String>();
@@ -30,6 +50,25 @@ public class Controller {
         }
         ObservableList<String> list = FXCollections.observableArrayList(modelList);
         comboExperimentCarModel.setItems(list);
+
+        //
+        comboExperimentCarModel.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (oldValue != newValue) {
+                    Car newCar = carDAO.getByModel(newValue);
+                    txValueMass.setText(""+newCar.getMass());
+                    txValuePower.setText(""+newCar.getPower());
+                    txValueDragCoefficient.setText(""+newCar.getDragCoefficient());
+                    txValueFrontalArea.setText(""+newCar.getFrontalArea());
+                }
+            }
+        });
+    }
+
+    @FXML
+    public void handleComboExperimentCarModelAction(ActionEvent event) {
+
     }
 
     public Controller() {}
--- src/main/java/br/hybridlab/standalone/dao/CarDAO.java
+++ src/main/java/br/hybridlab/standalone/dao/CarDAO.java
@@ -1,10 +1,20 @@
 package br.hybridlab.standalone.dao;
 
 import br.hybridlab.standalone.model.Car;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 /**
  * Created by alysson on 21/11/14.
  */
 public class CarDAO extends GenericDAO<Car> {
 
+    public Car getByModel(String model) {
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+        Car result = (Car) session.createCriteria(Car.class).add(Restrictions.eq("model",model)).uniqueResult();
+        transaction.commit();
+        return result;
+    }
 }
