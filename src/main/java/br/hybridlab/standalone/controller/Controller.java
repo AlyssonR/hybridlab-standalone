5b9cb64077a5212403f83d6e0e9f1f0536a4d0b2
Alysson Raniere <alysson_r076@hotmail.com>
2014-11-29 15:26:11 -0200

    Initializing car models comboBox

--- src/main/java/br/hybridlab/standalone/controller/Controller.java
+++ src/main/java/br/hybridlab/standalone/controller/Controller.java
@@ -2,12 +2,16 @@ package br.hybridlab.standalone.controller;
 
 import br.hybridlab.standalone.dao.CarDAO;
 import br.hybridlab.standalone.model.Car;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -17,14 +21,20 @@ public class Controller {
     private ComboBox<String> comboExperimentCarModel;
 
     @FXML
-    public void initialize() {}
+    public void initialize() {
+        comboExperimentCarModel.setPromptText("Escolha um modelo...");
+        List<Car> carList = carDAO.get();
+        ArrayList<String> modelList = new ArrayList<String>();
+        for (Car car : carList) {
+            modelList.add(car.getModel());
+        }
+        ObservableList<String> list = FXCollections.observableArrayList(modelList);
+        comboExperimentCarModel.setItems(list);
+    }
 
     public Controller() {}
 
     public Controller(CarDAO carDAO) {
         this.carDAO = carDAO;
     }
-
-
-
 }
